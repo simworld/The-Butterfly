da71f2160ef120a4833df82e1a2b68068d60a9f0
Simone Susino <simworld@tuta.io>
2023-07-18 09:12:38 +0100

    Nav and some CSS changes

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -3,9 +3,16 @@ window.addEventListener('scroll',function(){
     header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-function togglemenu() {
+function toggleMenu() {
     const menu = document.querySelector('.navigation');
     const menutoggle = document.querySelector('.menutoggle');
     menutoggle.classList.toggle('open');
     menu.classList.toggle('open');
+}
+
+
+function buttonLink(){
+    document.getElementsByClassName("btn")[0].addEventListener("click", function(){
+        window.scrollTo(0, document.querySelector(".parallax-form").offsetTop);
+    });
 }
