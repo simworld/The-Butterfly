8920273235c0fe5a79c80c97249aaae1f5fe7fd5
Simone Susino <simworld@tuta.io>
2023-07-25 10:50:24 +0100

    Minor changes, sections and video button

--- assets/js/video-controls.js
+++ assets/js/video-controls.js
@@ -4,9 +4,9 @@ let playPauseButton = document.getElementById("playPauseButton");
 function togglePlayPause() {
   if (video.paused) {
     video.play();
-    playPauseButton.className = "fas fa-pause";
+    playPauseButton.className = "fas fa-pause fa-3x";
   } else {
     video.pause();
-    playPauseButton.className = "fas fa-play";
+    playPauseButton.className = "fas fa-play fa-3x";
   }
 }
